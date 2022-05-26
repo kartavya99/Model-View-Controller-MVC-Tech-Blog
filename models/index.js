a2f950294961c4aeadd829c0dfafb688bf0f33c4
kartavya <kartavyas@gmail.com>
2022-05-26 23:21:39 +1000

    readme updated with ER diagram

--- models/index.js
+++ models/index.js
@@ -1,31 +1,37 @@
 const User = require("./user");
 const Post = require("./post");
-const comment = require("./comment");
+const Comment = require("./comment");
 
+// one to many
 User.hasMany(Post, {
   foreignKey: "user_id",
 });
 
+//one to many
 User.hasMany(Comment, {
   foreignKey: "user_id",
   onDelete: "cascade",
 });
 
+//one to one
 Post.belongsTo(User, {
   foreignKey: "user_id",
   onDelete: "cascade",
 });
 
+// one to many
 Post.hasMany(Comment, {
   foreignKey: "post_id",
   onDelete: "cascade",
 });
 
+//one to one
 Comment.belongsTo(User, {
   foreignKey: "user_id",
   onDelete: "cascade",
 });
 
+// one to one
 Comment.belongsTo(Post, {
   foreignKey: "post_id",
   onDelete: "cascade",
