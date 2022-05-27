1d7201012fd89f0707bec21f6247534924eda933
kartavya <kartavyas@gmail.com>
2022-05-27 21:26:17 +1000

    post route updated

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -48,7 +48,49 @@ router.get("/;id", async (req, res) => {
 });
 
 //update post
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedPost = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (!updatedPost) {
+      res.status(404).json({ message: " No post found with this id" });
+      return;
+    }
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //delete post
+router.delete("/:id", async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!postData) {
+      res.status(404).json({
+        message: `No post owned by user_id = ${req.session.user_id} found with id = ${req.params.id}`,
+      });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
