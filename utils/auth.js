bf51fb5cee334c957a78ff71eb014ae21a6d25fa
kartavya <kartavyas@gmail.com>
2022-05-27 17:10:41 +1000

    files added

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// middleware to verify user logged in before entering authorized route
+const withAuth = (req, res, next) => {
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
