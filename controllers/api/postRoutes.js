c28d7685f967663657d88f41ed528841c57d00af
kartavya <kartavyas@gmail.com>
2022-05-27 21:18:01 +1000

    updated files

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,1 +1,54 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+// get all Post
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      attributes: ["id", "title", "content", "created_at"],
+      order: [["created_at", "DESC"]],
+      include: [
+        { model: User, attributes: ["username"] },
+        {
+          model: Comment,
+          attribute: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        },
+      ],
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// get post by id
+router.get("/;id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk({
+      where: { id: req.params.id },
+      attribute: ["id", "title", "content", "created_at"],
+      order: [["created_at", "DEC"]],
+      include: [
+        { model: User, attributes: ["username"] },
+        {
+          model: Comment,
+          attribute: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        },
+      ],
+    });
+    if (!postData) {
+      res.status(404).json({ message: `no posts with id ${req.params.id}` });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//update post
+
+//delete post
+
 module.exports = router;
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,14 @@
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector("#name-signup").value.trim();
+  const username = document.querySelector("#name-signup").value.trim();
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
   if (name && email && password) {
     const response = await fetch("api/user", {
       method: "POST",
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ username, email, password }),
       headers: { "content-Type": "application/json" },
     });
 
