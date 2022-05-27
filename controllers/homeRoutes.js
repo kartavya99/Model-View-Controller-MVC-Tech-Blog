b8541ea96e5509d97204175ccd894047e4bc71cd
kartavya <kartavyas@gmail.com>
2022-05-27 12:09:43 +1000

    updated homeRoute and added more handlebars file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,7 +19,32 @@ router.get("/", async (req, res) => {
     const posts = postData.map((posts) => postData.get({ plain: true }));
 
     // Pass serialized data
-    res.render("homepage");
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get Post by id
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "comment_text", "post_id", "user_id"],
+          include: { model: User, attributes: ["username"] },
+        },
+      ],
+    });
+
+    //Serialize data so the template can read it
+    const posts = postData.get({ plain: true });
+
+    res.redirect("single-post");
   } catch (err) {
     res.status(500).json(err);
   }
