bc91f339f37fc78e3d562bf8a3e91dfab6f14b12
kartavya <kartavyas@gmail.com>
2022-05-27 22:13:08 +1000

    updated comment route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,1 +1,49 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+//get all comments
+router.get("/", async (req, res) => {
+  try {
+    const commentData = await Comment.findAll();
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// creat a new commnet
+router.get("/", async (req, res) => {
+  try {
+    const commentData = await Comment.create({
+      comment_text: req.session.comment_text,
+      post_id: req.session.post_id,
+      user_id: req.session.user.id,
+    });
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete comment
+router.delete("/:id", async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!commentData) {
+      res
+        .status(404)
+        .json({ message: `No post owned by user-id = ${req.session.user_id}` });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
