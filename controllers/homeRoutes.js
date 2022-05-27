a9feb726fec77c0a14cab383d42b895cd1c99a1c
kartavya <kartavyas@gmail.com>
2022-05-27 14:18:17 +1000

    homepage, login, singup and post by id route working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -50,4 +50,16 @@ router.get("/post/:id", async (req, res) => {
   }
 });
 
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
 module.exports = router;
