a2494a39e3f3b56f7b8d1720e8438de3303b17ab
kartavya <kartavyas@gmail.com>
2022-06-02 14:22:04 +1000

    updated files

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
+const helpers = require("./utils/helpers");
 const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 //setting up sessions with cookies
 const sess = {
