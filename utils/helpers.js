adfec8f73900f6923f1cce93b512c411c6f3e189
kartavya <kartavyas@gmail.com>
2022-06-02 19:36:11 +1000

    files updated

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,13 @@
 module.exports = {
-  format_date: (date) => {
+  format_date: (currentDate) => {
     // Format date as MM/DD/YYYY
-    return `${new Date(date).getMonth() + 1}/${new Date(
-      date
-    ).getDate()}/${new Date(date).getFullYear()}`;
+    // return `${
+    //   new Date().getMonth() + 1
+    // }/${new Date().getDate()}/${new Date().getFullYear()}`;
 
-    // date.toLocaleDateString();
+    const currentDates = new Date(currentDate);
+
+    return currentDates.toLocaleDateString();
   },
   format_amount: (amount) => {
     // format large numbers with commas
