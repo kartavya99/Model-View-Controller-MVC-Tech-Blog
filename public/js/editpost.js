a6fa4a90c2e454c975b0b898d3a27d320636f409
kartavya <kartavyas@gmail.com>
2023-04-04 17:33:17 +1000

    updated the functionality to retrieve the posts

--- public/js/editpost.js
+++ public/js/editpost.js
@@ -7,9 +7,15 @@ const editFormHandler = async (event) => {
 
   const content = document.querySelector("#post-content").value.trim();
 
-  const id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
-  ];
+  const updateButton = document.querySelector("#update-btn");
+
+  // const id = window.location.toString().split("/")[
+  //   window.location.toString().split("/").length - 1
+  // ];
+  // console.log(window.location.toString());
+  // console.log(window.location.toString().split("/"));
+  const id = updateButton.dataset.id;
+  console.log(updateButton.dataset.id);
 
   const response = await fetch(`/api/post/${id}`, {
     method: "PUT",
